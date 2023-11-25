7b307484022b6e77e708caa548e7809f72b93e29
milphaser <71965744+milphaser@users.noreply.github.com>
2023-11-25 20:16:40 +0200

    Project 3.HN ver.0.75 Update

---
+++ 03.HNG/1.Arsac/3.HN ver.0.75/3.uint32/UnitMain.h
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------------
+#ifndef UnitMainH
+#define UnitMainH
+//---------------------------------------------------------------------------
+//const int SCALE 	= 10;		//  Size of the generated HNS
+//const int SCALE 	= 24;		//  Size of the generated HNS
+//const int SCALE 	= 512;		//  Size of the generated HNS
+const int SCALE 	= 2048;		//  Size of the generated HNS
+//---------------------------------------------------------------------------
+using UINT = unsigned int;
+//---------------------------------------------------------------------------
+#endif
