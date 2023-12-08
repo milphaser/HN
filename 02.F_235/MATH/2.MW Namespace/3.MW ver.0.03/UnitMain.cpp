a563e69fd7994de50c7dfb6418e3fd268a18f556
milphaser <71965744+milphaser@users.noreply.github.com>
2023-12-08 09:53:22 +0200

    MW Namespace Refactoring

--- 02.F_235/MATH/2.MW Namespace/3.MW ver.0.03/UnitMain.cpp
+++ 02.F_235/MATH/2.MW Namespace/3.MW ver.0.03/UnitMain.cpp
@@ -13,7 +13,7 @@
 //---------------------------------------------------------------------------
 int _tmain(void)
 {
-	std::cout << "Multiplication of 64 bit multiplicand by 32 bit multiplier" << std::endl;
+	std::cout << "Multi-word Multiplication by 2, 3 and 5" << std::endl;
 	std::cout << "[Multi-word Namespace ver." << MW::ver << "]" << std::endl << std::endl;
 
 	MW::INT_T<4> tmp;   // 128 бит///////////////
